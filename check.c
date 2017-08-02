7afe1caba2f72f79e8a9d24839ba676e40ca1f12
vamsi <vamsi.tcp@gmail.com>
2017-08-02 11:04:31 +0530

    file modification 4

--- check.c
+++ check.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-   int a=30;
+   int a=10;
    int b=4;
    int c,d;
    c=a+b;
    d=a-b;
-  printf("the sum result is: %d",c);
-  printf("the sub result is:%d",d);
+  printf("the sum result is: %d\n",c);
+  printf("the sub result is:%d\n",d);
 
 }
