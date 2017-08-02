870127313d9098be06bafb6aec866b253d47cf43
satheesh <satheesh.narsingoju@yahoo.com>
2017-08-02 11:22:06 +0530

    multiplication added 1

--- check.c
+++ check.c
@@ -7,7 +7,7 @@ int main()
    int c,d;
    c=a+b;
    d=a-b;
-   v=a*b
+   v=a*b;
   printf("the sum result is: %d\n",c);
   printf("the sub result is:%d\n",d);
  printf("mulitplication of a and b is %d\n",v);
